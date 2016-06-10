51f181eab96232e31552d364973f25a610f6fae2
VinayBanakar <vinay.s.banakar@gmail.com>
2016-06-10 13:04:27 +0530

    uploading

---
+++ Employer/Header.php
@@ -0,0 +1,17 @@
+    <!-- Header -->
+    <div id="header">
+
+        <!-- Logotyp -->
+     
+        <hr class="noscreen" />          
+         <h2 style="font-family: hanken bold round;color: #9DB4F6; padding-left:300px; padding-top: 45px;">RECRUIT BOX</h2>
+        <!-- Quick links -->
+        <div class="noscreen noprint">
+            <p><em>Quick links: <a href="#content">content</a>, <a href="#tabs">navigation</a>, <a href="#search">search</a>.</em></p>
+            <hr />
+        </div>
+
+        <!-- Search -->
+       
+
+    </div> <!-- /header -->
---
+++ index.php
@@ -0,0 +1,107 @@
+<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+    
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+    
+    <title>RecruitBox</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+  /*<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+  */        
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+.style2 {
+	font-size: medium;
+	font-weight: bold;
+}
+-->
+    </style>
+</head>
+
+<body id="www-url-cz">
+<!-- Main -->
+<div id="main" class="box">
+<?php 
+include "Header.php"
+?>
+<?php 
+include "menu.php"
+?>   
+<!-- Page (2 columns) -->
+    <div id="page" class="box">
+    <div id="page-in" class="box">
+
+        <div id="strip" class="box noprint">
+
+            <!-- RSS feeds -->
+            <hr class="noscreen" />
+
+            <!-- Breadcrumbs -->
+            <p id="breadcrumbs">&nbsp;</p>
+          <hr class="noscreen" />
+            
+        </div> <!-- /strip -->
+
+        <!-- Content -->
+        <div id="content">
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="">
+                <h2><span><a href="#">Welcome to RECRUITBOX</a></span></h2>
+               
+
+                <p>vddsihvsdo svd8shv sdv sdv sdpv sdnvusd asdj vudsv dph va hiov dbdi ud fuvaidsjv idsvsdiv sdiv sd vasvaov ervdhvidv; dsvsd v a;svasidovh sdaoiv ;asdv isdv aisdvasdve0df 0vadfjvdfivhad</p>
+              <p>fdbdddddddddddvsdv sdv sdvhi shdv ioasdvhasdv ia;dsv asd;v oasdv asi;odv sdv uv dfvi dfjvpad vd ivad vasdvsvsiodvi dsdv jpidsv 'sdv a'p vds viasdiv aisd visd vid svids ivsdopvpo sdvvvvvvsdv oaso dvos dvo9wev'apdsvj dsj</p>
+              
+              <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+            <hr class="noscreen" />
+            
+        </div> <!-- /content -->
+
+<?php
+include "right.php"
+?>
+
+    </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+ 
+<?php
+include "footer.php"
+?>
+</div> <!-- /main -->
+
+</body>
+</html>
